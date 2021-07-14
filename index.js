834fa83a2e06bee4cac6241b9896e0e1f6be8a08
Anurag Singh <anuragsinghbam@gmail.com>
2021-07-14 16:12:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburgerMenu = document.querySelector('.hamburger')
+const navMenu = document.querySelector('.navbar ul')
+const overlay = document.querySelector('.overlay')
+
+console.log(overlay);
+
+hamburgerMenu.addEventListener('click', (e) => {
+  navMenu.classList.toggle('active')
+  overlay.classList.toggle('active')
+})
+
+overlay.addEventListener('click', (e) => {
+  navMenu.classList.remove('active')
+  overlay.classList.remove('active')
+})
